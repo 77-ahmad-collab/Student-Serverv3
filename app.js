c2b7efb291e7dc7f6b1fbabaa4c7dec07f1ccba9
77-ahmad-collab <ahmaq098@gmail.com>
2021-10-24 10:18:35 +0500

    routes has been added

--- app.js
+++ app.js
@@ -10,6 +10,6 @@ app.use(express.json());
 app.use(express.urlencoded({extended: false}));
 app.use("/", router);
 
-app.listen(5000, () => {
+app.listen(PORT, () => {
   console.log("app is listening on port 5000");
 });
