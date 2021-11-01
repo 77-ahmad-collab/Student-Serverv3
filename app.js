9c12fab6f2b0f244ba2c99b891545c9e5c0c7d39
77-ahmad-collab <ahmaq098@gmail.com>
2021-11-01 23:16:55 +0500

    server cors issue solved again

--- app.js
+++ app.js
@@ -11,6 +11,6 @@ app.use(express.json());
 app.use(express.urlencoded({extended: false}));
 app.use("/", router);
 app.use(cors());
-app.listen(5000, () => {
+app.listen(PORT, () => {
   console.log("app is listening on port 5000");
 });
